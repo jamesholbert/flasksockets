b633cb374ce4e2403d5e71b06b4df110283bc41a
James Holbert <arthesius@gmail.com>
2018-03-22 19:41:47 -0700

    refactor file restructure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 var DIST_DIR = path.resolve(__dirname,"dist");
-var SRC_DIR = path.resolve(__dirname,"templates");
+var SRC_DIR = path.resolve(__dirname,"src");
 
 var config = {
 	entry: SRC_DIR + "/app/index.js",
