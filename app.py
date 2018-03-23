70b882d2efcb5aa651a53e44ad26f36833d858d2
James Holbert <arthesius@gmail.com>
2018-03-22 20:52:08 -0700

    app running

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 # from __future__ import print_function
 # import sys #these top two are just for printing
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask_socketio import SocketIO
 
 import os
-# import filler
+
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/')
-application = Flask(__name__, template_folder=tmpl_dir)
+app = Flask(__name__, template_folder=tmpl_dir)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-@application.route("/")
-@application.route("/index")
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html') 
+	print('yo')
+	return render_template('index.html') 
 
-# @application.errorhandler(404)
+# @app.errorhandler(404)
 # def page_not_found(e):
 # 	return render_template('404.html'), 404
 
-# @application.errorhandler(500)
+# @app.errorhandler(500)
 # def server_error(e):
 # 	return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
-    # application.run()    
+    # app.run(debug=True)
+	socketio.run(app)
+
+    # app.run()    
